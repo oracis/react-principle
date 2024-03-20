4cbddcb15c7047ca3bc2af4d9c7da42e243f03a0
Robin Zhou <Robin.Zhou@burberry.com>
2024-03-20 18:02:28 +0800

    05. Concurrent Mode实现思路

--- public/index.js
+++ public/index.js
@@ -1,3 +1,19 @@
+let nextUnitWork = {};
+
+function performUnitWork(nextUnitWork) {
+  console.log(nextUnitWork);
+  return null;
+}
+
+function workLoop(deadLine) {
+  while (nextUnitWork) {
+    nextUnitWork = performUnitWork(nextUnitWork);
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
 const element = (
   <div title="Hello Welcome">
     <p>Robin</p>
